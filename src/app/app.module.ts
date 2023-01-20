276e307aaa7f041a410d09818e12bf55eb896f6e
piotrowczarczyk <powczarczyk@vp.pl>
2023-01-20 15:12:31 +0100

    showcase 3.1 - do we need to style :host element?

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,17 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { GrandParentComponent } from './grand-parent/grand-parent.component';
 import { ParentComponent } from './parent/parent.component';
 import { ChildComponent } from './child/child.component';
+import { ChildNoBindingComponent } from './child-no-binding/child-no-binding.component';
 
 @NgModule({
   imports: [BrowserModule, FormsModule],
   declarations: [
     AppComponent,
-    GrandParentComponent,
     ParentComponent,
     ChildComponent,
+    ChildNoBindingComponent,
   ],
   bootstrap: [AppComponent],
 })
