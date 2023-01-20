34058737ff439db825d50912cfdc5dd54a046108
piotrowczarczyk <powczarczyk@vp.pl>
2023-01-20 15:12:31 +0100

    showcase 3.1 - do we need to style :host element?

---
+++ src/app/child-no-binding/child-no-binding.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-child-no-binding',
+  templateUrl: './child-no-binding.component.html',
+  styleUrls: ['./child-no-binding.component.scss']
+})
+export class ChildNoBindingComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
