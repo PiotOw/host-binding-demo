b3f45f57245baf38aaca4d82ce08caa1b246e032
piotrowczarczyk <powczarczyk@vp.pl>
2023-01-20 04:58:49 +0100

    showcase 2 fix - HostBinding introducing multiple sources of truth

--- src/app/child/child.component.ts
+++ src/app/child/child.component.ts
@@ -1,4 +1,4 @@
-import { Component, HostBinding, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-child',
@@ -7,8 +7,7 @@ import { Component, HostBinding, Input, OnInit } from '@angular/core';
 })
 export class ChildComponent implements OnInit {
 
-  @HostBinding(`class.blue`)
-  private _blue = false;
+  public _blue = false;
 
   @Input() set blue(isBlue: boolean) {
     this._blue = isBlue;
