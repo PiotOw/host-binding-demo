26d1527d081688b6606ae8d400c30e96873a48ad
piotrowczarczyk <powczarczyk@vp.pl>
2023-01-23 08:24:36 +0100

    remove redundant code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { GrandParentComponent } from './grand-parent/grand-parent.component';
 import { ParentComponent } from './parent/parent.component';
 import { ChildComponent } from './child/child.component';
 
@@ -11,7 +10,6 @@ import { ChildComponent } from './child/child.component';
   imports: [BrowserModule, FormsModule],
   declarations: [
     AppComponent,
-    GrandParentComponent,
     ParentComponent,
     ChildComponent,
   ],
